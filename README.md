967664a860e82299c21c3ce8a3778bbe570c10b2
Matthias Andrasch <matthias.andrasch@dotsandlines.io>
2023-06-12 10:06:33 +0200

    chore: init

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # ddev-codespaces-vite
 Simple test repo for exploring vite support
+
+Status: WIP
+
+## Try
+
+1. Create new Codespaces instance
+2. Run `ddev npm ddev`
+
+-> We need to get a connection to the vite port and /src/js/app.js file
+## How was this created?
+
+```bash
+ddev config --project-type=php
+ddev start
+ddev npm init -y 
+ddev npm i -D vite
+
+# add expose_ports to .ddev/config.yaml 
+ddev restart
+
+# added scripts section to package.json
+
+# added .devcontainer files
+```
---
+++ vite.config.js
@@ -0,0 +1,40 @@
+// https://github.com/vitejs/vite/pull/677#issuecomment-1473740472
+const codespaceName = process.env['CODESPACE_NAME'];
+const codespaceDomain = process.env['GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN'];
+const hmrPort = 5173;
+
+const hmrRemoteHost = codespaceName ? `${codespaceName}-${hmrPort}.${codespaceDomain}` : 'localhost';
+const hmrRemotePort = codespaceName ? 443 : hmrPort;
+const hmrRemoteProtocol = codespaceName ? 'wss' : 'ws';
+
+console.log({codespaceName, codespaceDomain, hmrRemoteHost, hmrRemotePort, hmrRemoteProtocol});
+
+// Last step is to set 5173 to PUBLIC in the codespace, else you will get a CORS error. 
+// Unfortunately you cannot specify this in the .devcontainer
+
+export default ({ command }) => ({
+    // 'vite' is always command = 'serve', other command is 'build'
+    base: command === 'serve' ? '' : '/dist/',
+    build: {
+        manifest: true,
+        // outDir: '../cms/web/dist/',
+        rollupOptions: {
+            input: {
+                app: './src/js/app.js',
+            }
+        },
+    },
+    // for ddev:
+    server: {
+        host: '0.0.0.0', // react to all incoming traffic
+        port: 5173,
+        // new - for codespaces:
+        // TODO: check if this works locally in DDEV as well...
+        hmr: {
+            protocol: hmrRemoteProtocol,
+            host: hmrRemoteHost,
+            port: hmrPort,
+            clientPort: hmrRemotePort
+        }
+    }
+});
